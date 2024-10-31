33418151939c960412e89820dc1cb6fc540a57cb
Jen Warman <jen.warman85@gmail.com>
2024-10-31 14:19:04 +0000

    projects page layout and css

--- src/components/DesignProjects.jsx
+++ src/components/DesignProjects.jsx
@@ -47,6 +47,23 @@ function DesignProjects() {
         </Col>
       </Row>
 
+      <Row className="project-info">
+      <Col xs={5} md={3}>
+          <h2 className='recent-projects-title'>Project Name</h2>
+          <p className='recent-projects-text'>ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor elit odio, nec dapibus orci malesuada a. Ut gravida, lorem eget mattis porta, ex enim condimentum erat, ac lacinia turpis nibh at ipsum.</p>
+          <Button variant="outline-dark">Project</Button>
+        </Col>
+        <Col className='project-image' xs={5} md={3}>
+          <Image src="../iPhone.png" rounded fluid/>
+        </Col>
+        <Col className='project-image' xs={5} md={3}>
+          <Image src="../iPhone.png"rounded fluid/>
+        </Col>
+        <Col className='project-image' xs={5} md={3}>
+          <Image src="../iPhone.png" rounded fluid/>
+        </Col>
+      </Row>
+
       <Row>
         <Col>
         <div className="divider"></div></Col>
--- src/components/DeveloperProjects.jsx
+++ src/components/DeveloperProjects.jsx
@@ -8,17 +8,13 @@ function DeveloperProjects() {
         <Container className="design-projects">
         <Row>
             <Col>
-            <h1 className="project-header">Projects</h1>
-            <h2 className='recent-projects-title'>Software Development</h2>
+            <h1 className="developer-project-header">Projects</h1>
+            <h2 className='developer-recent-projects-title'>Software Development</h2>
             </Col>
         </Row>
         
         <Row className="project-info">
-      <Col xs={5} md={3}>
-          <h2 className='recent-projects-title'>Project Name</h2>
-          <p className='recent-projects-text'>ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor elit odio, nec dapibus orci malesuada a. Ut gravida, lorem eget mattis porta, ex enim condimentum erat, ac lacinia turpis nibh at ipsum.</p>
-          <Button variant="outline-dark">Project</Button>
-        </Col>
+      
         <Col className='project-image' xs={5} md={3}>
           <Image src="../iPhone.png" rounded fluid/>
         </Col>
@@ -27,15 +23,16 @@ function DeveloperProjects() {
         </Col>
         <Col className='project-image' xs={5} md={3}>
           <Image src="../iPhone.png" rounded fluid/>
+        </Col>
+        <Col xs={5} md={3}>
+          <h2 className='developer-projects-title'>Project Name</h2>
+          <p className='developer-projects-text'>ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor elit odio, nec dapibus orci malesuada a. Ut gravida, lorem eget mattis porta, ex enim condimentum erat, ac lacinia turpis nibh at ipsum.</p>
+          <Button className='developer-button' variant="outline-dark">Project</Button>
         </Col>
       </Row>
 
       <Row className="project-info">
-      <Col xs={5} md={3}>
-          <h2 className='recent-projects-title'>Project Name</h2>
-          <p className='recent-projects-text'>ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor elit odio, nec dapibus orci malesuada a. Ut gravida, lorem eget mattis porta, ex enim condimentum erat, ac lacinia turpis nibh at ipsum.</p>
-          <Button variant="outline-dark">Project</Button>
-        </Col>
+      
         <Col className='project-image' xs={5} md={3}>
           <Image src="../iPhone.png" rounded fluid/>
         </Col>
@@ -44,6 +41,11 @@ function DeveloperProjects() {
         </Col>
         <Col className='project-image' xs={5} md={3}>
           <Image src="../iPhone.png" rounded fluid/>
+        </Col>
+        <Col xs={5} md={3}>
+          <h2 className='developer-projects-title'>Project Name</h2>
+          <p className='developer-projects-text'>ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor elit odio, nec dapibus orci malesuada a. Ut gravida, lorem eget mattis porta, ex enim condimentum erat, ac lacinia turpis nibh at ipsum.</p>
+          <Button className='developer-button' variant="outline-dark">Project</Button>
         </Col>
       </Row>
       </Container>
