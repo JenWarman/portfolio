98d3704743c5b954805599a5ac606ea4ff8cffa4
Jen Warman <jen.warman85@gmail.com>
2024-10-31 11:56:13 +0000

    tidied files

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,22 +10,17 @@ function Skills() {
       <Row className='justify-content-center'>
         <Col xs={4} md={2}>
         <div className='skills-circle' id='figma'>Figma</div>
-          {/* <Image src="./figma.png" roundedCircle /> */}
         </Col>
         <Col xs={4} md={2}>
-          {/* <Image src="./adobe.png" roundedCircle /> */}
           <div className='skills-circle' id='adobe'>Adobe Creative Suite</div>
         </Col>
         <Col xs={4} md={2}>
-          {/* <Image src="./JavaScript.png" roundedCircle /> */}
           <div className='skills-circle' id='javascript'>JavaScript</div>
         </Col>
         <Col xs={4} md={2} >
-          {/* <Image src="./HTML.png" roundedCircle /> */}
           <div className='skills-circle' id='html'>HTML</div>
         </Col>
         <Col xs={4} md={2}>
-          {/* <Image src="./CSS.png" roundedCircle /> */}
           <div className='skills-circle' id='css'>CSS</div>
         </Col>
       </Row>
