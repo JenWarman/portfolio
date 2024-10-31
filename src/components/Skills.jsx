6143ded40f8d01d9701eaeed386f6be1719bfa83
Jen Warman <jen.warman85@gmail.com>
2024-10-31 10:14:49 +0000

    skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,31 @@
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+
 function Skills() {
-    return (
-        <h2>here are my super wicked skills</h2>
-    )
+
+  return (
+    <Container className='skills'>
+      <Row className='justify-content-center'>
+        <Col xs={4} md={2}>
+          <Image src="./figma.png" roundedCircle />
+        </Col>
+        <Col xs={4} md={2}>
+          <Image src="./adobe.png" roundedCircle />
+        </Col>
+        <Col xs={4} md={2}>
+          <Image src="./JavaScript.png" roundedCircle />
+        </Col>
+        <Col xs={4} md={2} >
+          <Image src="./HTML.png" roundedCircle />
+        </Col>
+        <Col xs={4} md={2}>
+          <Image src="./CSS.png" roundedCircle />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Skills;
