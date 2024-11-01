638847f363074310ffca68d745c26dbd1b08fc55
Jen Warman <jen.warman85@gmail.com>
2024-11-01 15:00:04 +0000

    media queries for home page

--- src/components/HeroBar.jsx
+++ src/components/HeroBar.jsx
@@ -9,7 +9,7 @@ function HeroBar() {
     <div className='diagonal-box'></div> 
     <div className="content">
     <Container className="hero-bar">
-      <Row>
+      <Row className='hero-row'>
       <Col xs={6} md={3}>
           <h1 id='designer' className='hero-header'>Designer</h1>
           <h5 className='hero-sub-header'>UI/UX design for engaging and functional digital experiences. </h5>
@@ -21,6 +21,16 @@ function HeroBar() {
           <h1 className='hero-header'>Developer</h1>
           <h5 className='hero-sub-header'>Frontend web development with solid and scaleable code. </h5>
         </Col>
+        <Row>
+        <Col>
+          <h1 className='media-hero-header'>Designer & Developer</h1>
+          </Col>
+        </Row>
+        <Row>
+        <Col>
+          <p className='media-hero-sub-header'>ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor elit odio, nec dapibus orci malesuada a. Ut gravida, lorem eget mattis porta, ex enim condimentum erat, ac lacinia turpis nibh at ipsum.</p>
+          </Col>
+        </Row>
       </Row>
     </Container>
     </div>
--- src/components/RecentProjects.jsx
+++ src/components/RecentProjects.jsx
@@ -7,7 +7,7 @@ import Button from 'react-bootstrap/Button';
 function RecentProjects() {
   return (
     <Container className="recent-projects">
-      <Row>
+      <Row className='recent-projects-row'>
       <Col xs={5} md={3}>
           <h2 className='recent-projects-title'>Recent Projects</h2>
           <p className='recent-projects-text'>ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor elit odio, nec dapibus orci malesuada a. Ut gravida, lorem eget mattis porta, ex enim condimentum erat, ac lacinia turpis nibh at ipsum.</p>
