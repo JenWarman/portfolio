cd41ce5820e2c9a44235037409d87b2a1dfceda5
Jen Warman <jen.warman85@gmail.com>
2024-11-04 10:36:53 +0000

    projects page css for media queries

--- src/components/DeveloperProjects.jsx
+++ src/components/DeveloperProjects.jsx
@@ -15,16 +15,16 @@ function DeveloperProjects() {
         
         <Row className="project-info">
       
-        <Col className='project-image' xs={5} md={3}>
+        <Col className='project-image' xs={6} md={3}>
           <Image src="../iPhone.png" rounded fluid/>
         </Col>
-        <Col className='project-image' xs={5} md={3}>
+        <Col className='project-image' xs={6} md={3}>
           <Image src="../iPhone.png"rounded fluid/>
         </Col>
-        <Col className='project-image' xs={5} md={3}>
+        <Col className='project-image' xs={6} md={3}>
           <Image src="../iPhone.png" rounded fluid/>
         </Col>
-        <Col xs={5} md={3}>
+        <Col xs={6} md={3}>
           <h2 className='developer-projects-title'>Project Name</h2>
           <p className='developer-projects-text'>ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor elit odio, nec dapibus orci malesuada a. Ut gravida, lorem eget mattis porta, ex enim condimentum erat, ac lacinia turpis nibh at ipsum.</p>
           <Button className='developer-button' variant="outline-dark">Project</Button>
@@ -33,16 +33,16 @@ function DeveloperProjects() {
 
       <Row className="project-info">
       
-        <Col className='project-image' xs={5} md={3}>
+        <Col className='project-image' xs={6} md={3}>
           <Image src="../iPhone.png" rounded fluid/>
         </Col>
-        <Col className='project-image' xs={5} md={3}>
+        <Col className='project-image' xs={6} md={3}>
           <Image src="../iPhone.png"rounded fluid/>
         </Col>
-        <Col className='project-image' xs={5} md={3}>
+        <Col className='project-image' xs={6} md={3}>
           <Image src="../iPhone.png" rounded fluid/>
         </Col>
-        <Col xs={5} md={3}>
+        <Col xs={6} md={3}>
           <h2 className='developer-projects-title'>Project Name</h2>
           <p className='developer-projects-text'>ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor elit odio, nec dapibus orci malesuada a. Ut gravida, lorem eget mattis porta, ex enim condimentum erat, ac lacinia turpis nibh at ipsum.</p>
           <Button className='developer-button' variant="outline-dark">Project</Button>
