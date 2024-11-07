536a3f24b1abd9dfadd10d66addb320828f860a5
Jen Warman <jen.warman85@gmail.com>
2024-11-07 13:39:42 +0000

    link css

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
           <Route path='/' element={<><HeroBar /><RecentProjects/></>}></Route>
             <Route path='/about' element={<><About /><Skills/></>}></Route>
             <Route path='/projects' element={<><DesignProjects /><DeveloperProjects/></>}></Route>
-            {/* <Route path='/byte-size-habits' element={<ByteSizeHabits></ByteSizeHabits>}></Route> */}
+            <Route path='/byte-size-habits' element={<ByteSizeHabits></ByteSizeHabits>}></Route>
         </Routes> 
       <Footer />
     </div>
--- src/components/RecentProjects.jsx
+++ src/components/RecentProjects.jsx
@@ -3,15 +3,15 @@ import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
-import  Link  from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function RecentProjects() {
   return (
     <Container className="recent-projects">
       <Row className='recent-projects-row'>
       <Col xs={5} md={3}>
-          <h2 className='recent-projects-title'>Recent Projects</h2>
-          <p className='recent-projects-text'><strong>ByteSize Habits</strong>is a modern web application designed to help users track their daily habits and personal goals, with a playful, Tamagotchi-style interface. Created using React, Next.js, and styled with TailwindCSS and DaisyUI, the app allows users to set goals, track progress, and interact with a cute, tech-themed character.</p>
+         <h2 className='recent-projects-title'><Link className='recent-projects-link' to='/projects'>Recent Projects</Link></h2>
+          <p className='recent-projects-text'><Link className='recent-projects-link' to='/byte-size-habits'><strong>Byte Size Habits</strong></Link> is a modern web application designed to help users track their daily habits and personal goals, with a playful, Tamagotchi-style interface. Created using React, Next.js, and styled with TailwindCSS and DaisyUI, the app allows users to set goals, track progress, and interact with a cute, tech-themed character.</p>
           <Button className='recent-projects-button' variant="outline-dark">Project</Button>
         </Col>
         <Col className='recent-project-image' xs={5} md={3}>
