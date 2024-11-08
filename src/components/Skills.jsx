dd4611c331c259d155fb4b675deb1904b524849d
Jen Warman <jen.warman85@gmail.com>
2024-11-08 15:35:52 +0000

    update skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,21 +8,56 @@ function Skills() {
 
   return (
     <Container className='skills'>
+      <Row>
+      <h2 className='skills-title'>Design</h2>
+      </Row>
       <Row className='skills-row'>
         <Col xs={4} md={2} >
         <div className='skills-circle' id='figma'>Figma</div>
         </Col>
         <Col xs={4} md={2} >
-          <div className='skills-circle' id='adobe'>Adobe Creative Suite</div>
+          <div className='skills-circle' id='adobe'>Adobe Photoshop</div>
         </Col>
         <Col xs={4} md={2} >
-          <div className='skills-circle' id='javascript'>JavaScript</div>
+          <div className='skills-circle' id='javascript'>Adobe Illustrator</div>
         </Col>
-        <Col xs={4} md={2} >
+        {/* <Col xs={4} md={2} >
           <div className='skills-circle' id='html'>HTML</div>
         </Col>
         <Col xs={4} md={2} >
           <div className='skills-circle' id='css'>CSS</div>
+        </Col> */}
+        </Row>
+        <Row>
+      <h2 className='skills-title' id='development-skills-title'>Development</h2>
+      </Row>
+        <Row className='skills-row'>
+        <Col xs={4} md={2} >
+        <div className='skills-circle' id='figma'>JavaScript</div>
+        </Col>
+        <Col xs={4} md={2} >
+          <div className='skills-circle' id='adobe'>HTML</div>
+        </Col>
+        <Col xs={4} md={2} >
+          <div className='skills-circle' id='javascript'>CSS</div>
+        </Col>
+        <Col xs={4} md={2} >
+          <div className='skills-circle' id='html'>React.js</div>
+        </Col>
+        <Col xs={4} md={2} >
+          <div className='skills-circle' id='css'>Next.js</div>
+        </Col>
+        <Col xs={4} md={2} >
+          <div className='skills-circle' id='figma'>SQL</div>
+        </Col>
+        <Col xs={4} md={2} >
+          <div className='skills-circle' id='adobe'>PostgreSQL</div>
+        </Col>
+        <Col xs={4} md={2} >
+          <div className='skills-circle' id='javascript'>Jest</div>
+        </Col>
+        <Col xs={4} md={2} >
+          <div className='skills-circle' id='html'>SuperTest</div>
         </Col>
         </Row>
     </Container>
