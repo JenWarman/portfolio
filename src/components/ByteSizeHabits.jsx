7f392885426de85ae8f9e82a9cd5547041fe162c
Jen Warman <jen.warman85@gmail.com>
2024-11-08 12:36:32 +0000

    media queries for byte size habits component

--- src/components/ByteSizeHabits.jsx
+++ src/components/ByteSizeHabits.jsx
@@ -5,17 +5,17 @@ function ByteSizeHabits() {
     return (
         <>
         <Container classname='byte-size-habits'>
-          <Row>
-            <Col className='byte-size-col'xs={4} md={4}>
+          <Row className='byte-size-row'>
+            <Col className='byte-size-col'xs={10} md={4}>
             <h1 className="byte-size-title"><Link className='recent-projects-link'to='https://byte-size-habits.vercel.app/'>Byte Size Habits</Link></h1>
             <h5 className="byte-size-intro"><Link className='recent-projects-link' to='https://github.com/DannyGreenan/byte-size-habits'><strong>Byte Size Habits</strong></Link> is a modern web application designed to help users track their daily habits and personal goals, with a playful, Tamagotchi-style interface. <br></br><br></br>Created using React, Next.js, and styled with TailwindCSS and DaisyUI, the app allows users to set goals, track progress, and interact with a cute, tech-themed character.</h5>
             </Col>
-            <Col xs={8} md={8}>
+            <Col xs={10} md={8}>
             <Image classname='byte-size-group-image'src='../ByteSizeGroupCropped.png'fluid />
             </Col>
           </Row>
-          <Row>
-            <Col xs={6} md={5}>
+          <Row className='byte-size-row'>
+            <Col xs={5} md={5}>
             <h2 className="byte-size-sub-header">Project</h2>
             <h5 className="byte-size-intro">Final group project for <Link className='recent-projects-link' to='https://northcoders.com/project-phase/byte-size-habits'><strong>Northcoders</strong></Link> bootcamp.</h5>
             <h2 className="byte-size-sub-header">Role</h2>
@@ -26,7 +26,7 @@ function ByteSizeHabits() {
               <li className="byte-size-list"><h5></h5></li>
             </ul>
             </Col>
-            <Col xs={6} md={6}><h2 className="byte-size-sub-header">Tech Stack</h2>
+            <Col xs={5} md={6}><h2 className="byte-size-sub-header">Tech Stack</h2>
             <ul>
               <li className="byte-size-list"><h5 className="byte-size-list">JavaScript</h5></li>
               <li className="byte-size-list"><h5 className="byte-size-list">Next.js</h5></li>
@@ -45,22 +45,22 @@ function ByteSizeHabits() {
               <h2 id='wireframe-header'>Wireframes</h2>
             </Row>
           <Row className="byte-size-wireframe-row">
-            <Col xs={2} md={4}>
+            <Col xs={4} md={4}>
             <Image className="wireframe-image" src='../wireframe1.png'/>
             </Col>
-            <Col xs={2} md={4}>
+            <Col xs={4} md={4}>
             <Image className="wireframe-image" src='../wireframe2.png'/>
             </Col>
-            <Col xs={2} md={4}>
+            <Col xs={4} md={4}>
             <Image className="wireframe-image"  src='../wireframe3.png'/>
             </Col>
-            <Col xs={2} md={4}>
+            <Col xs={4} md={4}>
             <Image className="wireframe-image" src='../wireframe4.png'/>
             </Col>
-            <Col xs={2} md={4}>
+            <Col xs={4} md={4}>
             <Image className="wireframe-image" src='../wireframe5.png'/>
             </Col>
-            <Col xs={2} md={4}>
+            <Col xs={4} md={4}>
             <Image className="wireframe-image" src='../wireframe6.png'/>
             </Col>
           </Row>
