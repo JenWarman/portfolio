1c6b428a81885c12773815f9b5195cc03b0d3f81
Jen Warman <jen.warman85@gmail.com>
2024-11-21 11:47:37 +0000

    tailwind css install and config

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/components/**/*.{html,jsx}",
+    "./src/App.jsx",
+    "./src/main.jsx",
+    "./public/**/*.{html,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
