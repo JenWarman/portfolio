22dea29f4a80cf6658e8d067d37f8fc06f996ada
Jen Warman <jen.warman85@gmail.com>
2024-11-22 15:57:46 +0000

    edits to projects container

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,24 +10,6 @@ function Projects() {
             <h1 className="project-header">Projects</h1>
           </Col>
         </Row>
-
-        {/* <Row className="project-info">
-      <Col xs={6} md={3}>
-          <Link to='/book-club-app' className='recent-projects-link'><h2 className='projects-title'>Book Club App</h2></Link>
-          <p className='projects-text'>ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor elit odio, nec dapibus orci malesuada a. Ut gravida, lorem eget mattis porta, ex enim condimentum erat, ac lacinia turpis nibh at ipsum.</p>
-          <Link to='/book-club-app'><Button className='project-button' variant="outline-dark">Project</Button></Link>
-        <Link><Image className="project-icon" src='../domain.png'/></Link>
-        </Col>
-        <Col className='project-image' xs={6} md={3}>
-          <Image src="../iPhone.png" rounded fluid/>
-        </Col>
-        <Col className='project-image' xs={6} md={3}>
-          <Image src="../iPhone.png"rounded fluid/>
-        </Col>
-        <Col className='project-image' xs={6} md={3}>
-          <Image src="../iPhone.png" rounded fluid/>
-        </Col>
-      </Row> */}
         <Row className="project-info">
           <Col xs={6} md={3}>
             <Link to="/byte-size-habits" className="recent-projects-link">
