4c27934bf720672fe18ef24d0b2b4c7d516003fb
Jen Warman <jen.warman85@gmail.com>
2024-11-29 12:06:01 +0000

    update to book-club component

--- src/components/BookClubApp.jsx
+++ src/components/BookClubApp.jsx
@@ -33,9 +33,9 @@ function BookClubApp() {
             <Row>
             <Col xs={10} md={12}><h4 className='book-club-intro'><strong>Your Book Club</strong> is an application that cultivates a community of book lovers and connects them with book clubs to share and discover new reading experiences. <br></br>
 
-<br></br>The user can create their own book club or search for open book clubs using their personalised search criteria, such as location, meet-up style (online or in person), book genre or theme. <br></br>
+<br></br>The user can create their own book club or search for open book clubs using their personalised search criteria, such as location, meet-up style (online or in-person), book genre or theme. <br></br>
 
-<br></br>User engagement is encouraged through access to group messaging, polls for future book club picks, an interactive calendar to arrange future meetings and awards for book club participation and completing reading challenges. 
+<br></br>User engagement is encouraged through access to group messaging, polls for future book club picks and awards for book club participation and completing reading challenges. 
 </h4></Col>
            </Row>
           <Row className='byte-size-row'>
@@ -53,24 +53,25 @@ function BookClubApp() {
             <h2 className="byte-size-sub-header">Objectives</h2>
           <ul>
               <li className="byte-size-list"><h5 className="byte-size-list"><span><strong>Cultivate connection.</strong> Your Book Club allows the user to create their own book club or join an existing one, nurturing connections between book lovers and allowing them to share their reading experiences. </span></h5></li>
-              <li className="byte-size-list"><h5 className="byte-size-list"><span><strong>Simplify and facilitate. </strong> This app provides group messaging for book clubs to promote book discussions and tools such as polls and an interactive calendar to plan future meetings. 
+              <li className="byte-size-list"><h5 className="byte-size-list"><span><strong>Simplify and facilitate. </strong> This app provides group messaging for book clubs to promote book discussions and tools such as polls to plan future meetings. 
               </span></h5></li>
-              <li className="byte-size-list"><h5 className="byte-size-list"><span><strong>Encourage user engagement.</strong> When the user completes a study session they are awarded coins in addition to restoring Nibbler’s health. These coins can be redeemed in store to treat your virtual pet.
+              <li className="byte-size-list"><h5 className="byte-size-list"><span><strong>Encourage user engagement.</strong> Your Book Club not only provides a connection to a community of book lovers it also rewards users with profile badges and incentives for their participation in book clubs and the completion of reading challenges. 
               </span></h5></li>
             </ul>
             </Col>
           </Row>
           <Row className='byte-size-row'>
             <Col xs={11} md={8} className="byte-size-objectives">
-            <h2 className="byte-size-sub-header">The User</h2>
+            <h2 className="byte-size-sub-header">Persona</h2>
           <ul>
-              <li className="byte-size-list"><h5 className="byte-size-list"><span><strong>Set personal goals.</strong> The user can select a programming language to learn and set a daily target to study, for example 30 minutes per day. </span></h5></li>
-              <li className="byte-size-list"><h5 className="byte-size-list"><span><strong>Motivate the user.</strong> Nibbler's health decreases over time and the only way to restore him to full charge is for the user to complete their daily learning objective.
-              </span></h5></li>
-              <li className="byte-size-list"><h5 className="byte-size-list"><span><strong>Reward the user. </strong> When the user completes a study session they are awarded coins in addition to restoring Nibbler’s health. These coins can be redeemed in store to treat your virtual pet.
-              </span></h5></li>
-              <li className="byte-size-list"><h5 className="byte-size-list"><span><strong> Track progress.</strong>The user can track their progress and compare their weekly or monthly streak.
-              </span></h5></li>
+              <li className="byte-size-list"><h5 className="byte-size-list"><span><strong>Name: </strong> Charlotte Hughes</span></h5></li>
+              <li className="byte-size-list"><h5 className="byte-size-list"><span><strong>Age: </strong>35</span></h5></li>
+              <li className="byte-size-list"><h5 className="byte-size-list"><span><strong>Occupation: </strong>Marketing Manager</span></h5></li>
+              <li className="byte-size-list"><h5 className="byte-size-list"><span><strong>Location: </strong>Birmingham</span></h5></li>
+              <li className="byte-size-list"><h5 className="byte-size-list"><span><strong>Device: </strong>Android (Pixel 6)</span></h5></li>
+              <li className="byte-size-list"><h5 className="byte-size-list"><span><strong>Desired Outcome: </strong>Meet new people and increase personal reading goals.</span></h5></li>
+              <li className="byte-size-list"><h5 className="byte-size-list"><span><strong>Strengths: </strong>Intelligent, professional and open-minded.</span></h5></li>
+            <li className="byte-size-list"><h5 className="byte-size-list"><span><strong>Weaknesses: </strong>Time management and limited social circle.</span></h5></li>
             </ul>
             </Col>
             <Col xs={11} md={4}><Image src='../profile.jpg' fluid/></Col>
