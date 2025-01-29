c3822420ef9000d42908bf9fc3172181aaf28e1c
Jen Warman <jen.warman85@gmail.com>
2025-01-29 16:18:28 +0000

    Active class to bytesize tabs.

--- src/components/ByteSizeHabits/ByteSizeTabs.jsx
+++ src/components/ByteSizeHabits/ByteSizeTabs.jsx
@@ -1,45 +1,75 @@
 import { useState } from "react";
 import ByteSizeWireFrames from "./ByteSizeWireFrames";
 import ByteSizeHabitsDemo from "./ByteSizeHabitsDemo";
-import { Container, Col, Row } from "react-bootstrap";
+import { Container, Col } from "react-bootstrap";
 
 export default function ByteSizeHabitsTabs() {
-  const [selectedTab, setSelectedTab] = useState();
- 
+  const [selectedTab, setSelectedTab] = useState("wireframes");
 
   function handleClick(event) {
     setSelectedTab(event.target.value);
-  
   }
 
+  let content;
+
+  if (selectedTab === "wireframes") {
+    content = <ByteSizeWireFrames />;
+  }
+  if (selectedTab === "demo") {
+    content = <ByteSizeHabitsDemo />;
+  }
+  if (selectedTab === "github") {
+    content = <p>Github</p>;
+  }
+  if (selectedTab === "website") {
+    content = <p>Website</p>;
+  }
   return (
-    <Container className="tabs">
-      <Row className="tabs-row">
-        <Col xs={6} md={2}>
-          <button
-            className="book-club-tab-button"
-            value="wireframes"
-            onClick={handleClick}
-          >
-            Wireframes
-          </button>
-        </Col>
-        <Col xs={6} md={1}>
-          <button
-            className="book-club-tab-button"
-            value="demo"
-            onClick={handleClick}
-          >
-            Demo
-          </button>
-        </Col>
-      </Row>
-      {!selectedTab && <p>Please select a tab</p>}
-      {selectedTab === "demo" ? (
-        <ByteSizeHabitsDemo />
-      ) : (
-        <ByteSizeWireFrames />
-      )}
+    <Container className="tabs" center>
+      <Col>
+        <button
+          value="wireframes"
+          onClick={handleClick}
+          className={
+            "book-club-tab-button " +
+            (selectedTab == "wireframes" ? "active " : "")
+          }
+        >
+          Wireframes
+        </button>
+
+        <button
+          className={
+            "book-club-tab-button " + (selectedTab == "demo" ? "active " : "")
+          }
+          value="demo"
+          onClick={handleClick}
+        >
+          Demo
+        </button>
+
+        <button
+          className={
+            "book-club-tab-button " + (selectedTab == "github" ? "active " : "")
+          }
+          value="github"
+          onClick={handleClick}
+        >
+          Github
+        </button>
+
+        <button
+          className={
+            "book-club-tab-button " +
+            (selectedTab == "website" ? "active " : "")
+          }
+          value="website"
+          onClick={handleClick}
+        >
+          Website
+        </button>
+      </Col>
+      {content}
     </Container>
   );
 }
