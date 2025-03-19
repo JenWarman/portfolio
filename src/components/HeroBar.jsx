82badb0685b6546b9d5d382925c49090a14fd709
Jen Warman <jen.warman85@gmail.com>
2025-03-19 12:03:16 +0000

    Herobar css.

--- src/components/HeroBar.jsx
+++ src/components/HeroBar.jsx
@@ -6,37 +6,23 @@ import Row from "react-bootstrap/Row";
 function HeroBar() {
   return (
     <Container>
-      <Row className="d-flex flex-row mb-3 justify-content-center align-items-center">
-        <Col className="slide-right ">
-          <h1 id="designer" className="display-1 hero-sub-header text-wrap fw-bold lh-base text-black text-center" >
-            Designer
-          </h1>
-          <h5 className="hero-sub-header text-wrap fs-3 fw-normal lh-base text-black text-justify">
+      <Row className="align-items-stretch py-5">
+        <Col className="align-items-center m-auto p-1">
+          <Col>
+            <h1 className="display-1 text-wrap fw-bold text-black pb-1 pt-1 text-center">
+              Designer & Developer
+            </h1>
+          </Col>
+          <Col></Col>
+          <p className="fs-3 fw-normal lh-base text-black text-justify align-text-center pt-3 ps-5 pe-5">
             UI/UX design for engaging and functional digital experiences.
-          </h5>
+            Engaging and functional UI/UX design for digital experiences and
+            frontend web development with solid and scalable code.
+          </p>
         </Col>
-        <Col>
-          <Image className="img-fluid" src="../profileBG.png" />
+        <Col xs="auto" className="d-flex flex-column justify-content-center">
+          <Image src="../profileBG.png" alt="Example" />
         </Col>
-        <Col className="slide-left">
-          <h1 className="display-1 hero-sub-header text-wrap fw-bold lh-base text-black text-center">Developer</h1>
-          <h5 className="hero-sub-header text-wrap fs-3 fw-normal lh-base text-black">
-            Frontend web development with solid and scalable code.
-          </h5>
-        </Col>
-        <Row>
-          <Col>
-            <h1 className="media-hero-header">Designer & Developer</h1>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <p className="media-hero-sub-header">
-              Engaging and functional UI/UX design for digital experiences and
-              frontend web development with solid and scalable code.
-            </p>
-          </Col>
-        </Row>
       </Row>
     </Container>
   );
