ef1554b3f99f67c37c339619c0677daf240f788b
Jen Warman <jen.warman85@gmail.com>
2025-04-08 15:51:31 +0100

    Not Art page updated.

--- src/components/NotArt.jsx
+++ src/components/NotArt.jsx
@@ -10,28 +10,29 @@ export default function NotArt() {
             <Image src="../../NotArt/NotArt_Main.png" fluid />
           </Row>
           <Row>
-            <h1 className="text-center display-2 fw-bold p-3">
+            <h1 className="text-center display-6 fw-bold p-3">
               <Link
                 target="_blank"
-                className="link-opacity-25-hover link-offset-2 link-underline link-underline-opacity-0 link-dark"
+                className="link-opacity-25-hover link-offset-2 link-underline link-underline-opacity-0 text-black"
                 to="https://not-art.netlify.app/"
               >
                 Not Art
               </Link>
             </h1>
-            <h5 className="about-text fs-4 text-black pb-3">
+            <h5 className="about-text fs-5 text-black pb-3">
               Welcome to
               <Link
                 target="_blank"
-                className="link-opacity-25-hover link-offset-2 link-underline link-underline-opacity-0 link-dark"
+                className="link-opacity-25-hover link-offset-2 link-underline link-underline-opacity-0 text-black"
                 to="https://github.com/JenWarman/events-platform"
               >
                 <strong> Not Art </strong>
               </Link>
-              a web application that allows members of Nottingham&#39;s creative
-              community find events, sign up to attend and add them to their own
-              personal calendar. Staff members can add, edit and delete events
-              from the platform.
+              a responsive web application for Nottingham&#39;s creative
+              community allowing members to search events, sign up to attend and
+              add them to their own personal calendar. Staff members can also
+              manage events by adding, editing and deleting events from the
+              platform.
             </h5>
           </Row>
         </Row>
@@ -39,26 +40,28 @@ export default function NotArt() {
           <Row>
             <Col>
               <Row>
-                <Col className="project-info">
-                  <h2 className="display-8 fw-bold">Project</h2>
-                  <h5 className="fs-4 text-black pb-3">
-                    A web application for a small creative business in
-                    Nottingham to create and share events with their members.
+                <Row className="project-info">
+                  <h2 className="fs-4 fw-bold">Project</h2>
+                  <h5 className="fs-5 text-black pb-3">
+                    A full stack web application for a creative business based
+                    in Nottingham to help manage and share events with their
+                    community members.
                   </h5>
-                </Col>
-                <Col className="project-info">
-                  <h2 className="display-8 fw-bold">Tech Stack</h2>
-                  <h5 className="fs-4 text-black pb-3">
+                </Row>
+                <Row className="project-info">
+                  <h2 className="fs-4 fw-bold">Tech Stack</h2>
+                  <h5 className="fs-5 text-black pb-3">
                     TypeScript, Angular, CSS and Supabase.
                   </h5>
-                </Col>
+                </Row>
               </Row>
-              <Col className="project-info">
-                <h2 className="display-8 fw-bold">Role</h2>
-                <h5 className="fs-4 text-black pb-3">
-                  Designer and frontend developer.
+              <Row className="project-info">
+                <h2 className="fs-4 fw-bold">Role</h2>
+                <h5 className="fs-5 text-black pb-3">
+                  Freelance Project Consultant responsible for UX/UI design and
+                  full stack software development.
                 </h5>
-              </Col>
+              </Row>
             </Col>
           </Row>
           <Row>
@@ -66,58 +69,60 @@ export default function NotArt() {
           </Row>
         </Row>
         <Row className="p-3">
-          <h2 className="display-8 fw-bold">Objectives</h2>
+          <h2 className="fs-4 fw-bold">Objectives</h2>
           <ul>
             <li className="byte-size-list">
-              <h5 className="fs-4 text-black">
+              <h5 className="fs-5 text-black">
                 <span>
-                  <strong>Engage the user.</strong> Easy search functionality
-                  and access to free art and cultural events throughout
-                  Nottingham.
+                  <strong>Engage the user.</strong> Access to local art and
+                  cultural events with easy search functionality allowing the
+                  user to search for events by category or keyword.
                 </span>
               </h5>
             </li>
             <li className="byte-size-list">
-              <h5 className="fs-4 text-black">
+              <h5 className="fs-5 text-black">
                 <span>
-                  <strong>Ease of use.</strong> The user can sign-up for a event
-                  with one click and add an event to their personal calendar.
+                  <strong>Ease of use.</strong> The user can sign-up to attend
+                  an event with a single click, edit their RSVP status via their
+                  profile and add an event to their personal calendar.
                 </span>
               </h5>
             </li>
             <li className="byte-size-list">
-              <h5 className="fs-4 text-black">
+              <h5 className="fs-5 text-black">
                 <span>
-                  <strong>Easy platform management.</strong> An admin user can
-                  create, edit and delete events.
+                  <strong>Event Management.</strong> The platform profides admin
+                  users with event management tools, allowing them to create,
+                  edit and delete events easily.
                 </span>
               </h5>
             </li>
           </ul>
         </Row>
         <Row className="p-3">
-          <h2 className="display-8 fw-bold">Application Features</h2>
+          <h2 className="fs-4 fw-bold">Application Features</h2>
           <ul>
             <li className="byte-size-list">
-              <h5 className="fs-4 text-black">
+              <h5 className="fs-5 text-black">
                 <span>
-                  <strong>Event Search.</strong> All users with or without an
+                  <strong>Search Functionality.</strong> All users with or without an
                   account can search for events by event type (e.g. music,
-                  theatre, etc) or using the search bar.
+                  theatre, etc) or using a keyword search in the search bar.
                 </span>
               </h5>
             </li>
             <li className="byte-size-list">
-              <h5 className="fs-4 text-black">
+              <h5 className="fs-5 text-black">
                 <span>
-                  <strong>User Login.</strong> Registered users can log in to
+                  <strong>User Verifcation.</strong> Registered users can log in to
                   sign up for events and access these events via their profile
                   page.
                 </span>
               </h5>
             </li>
             <li className="byte-size-list">
-              <h5 className="fs-4 text-black">
+              <h5 className="fs-5 text-black">
                 <span>
                   <strong>Google Calendar Integration.</strong> Once a user has
                   signed up for an event they will have the option to add this
@@ -126,7 +131,7 @@ export default function NotArt() {
               </h5>
             </li>
             <li className="byte-size-list">
-              <h5 className="fs-4 text-black">
+              <h5 className="fs-5 text-black">
                 <span>
                   <strong>Staff Authorisation.</strong> Admin users can manage
                   events by adding new events, editing event details and
